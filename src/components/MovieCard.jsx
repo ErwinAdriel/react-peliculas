c63f23d27a3f109053721ee167a79650a188608e
ErwinAdriel <adrielquispe87@gmail.com>
2024-04-29 22:50:35 -0300

    Mostrando imagen en MovieDetalle

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import "../components/MovieCard.css" 
 import { Link } from "react-router-dom";
 
+
 export function MovieCard({movie}){
     const imageUrl = "https://image.tmdb.org/t/p/w300"+movie.poster_path;
     
--- src/pages/MovieDetalles.jsx
+++ src/pages/MovieDetalles.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {get} from "../data/httpClient";
+import { getMovieImg } from "../utils/getMovieImg";
 export function MovieDetalles(){
-    return(
-        <h1>Detalles</h1>
-    )
+    
+    const {movieId} = useParams();
+    const [movie, setMovie] = useState([]);
+
+    useEffect(()=>{
+        get("/movie/"+movieId).then((data)=>{
+            setMovie(data)
+        });
+    }, [movieId]);
+
+    const imageUrl = getMovieImg(movie.poster_path, 500);
+    return (
+        <div>
+            <img src={imageUrl} />
+        </div>
+    );
 }
